4e29f722fc758a0d19be21dd696db2d379882cce
yejot1514 <banchihai@gmail.com>
2026-04-13 23:19:37 -0400

    completed ci build updated 5

--- service/routes.py
+++ service/routes.py
@@ -80,15 +80,13 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
-# ... place you code here to READ an account ...
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
     Reads an account
     This endpoint will read an Account based the account_id that is requested
     """
-    app.logger.info("Request to read an Account with id: %s" , account_id)
+    app.logger.info("Request to read an Account with id: %s", account_id)
 
     account = Account.find(account_id)
     if not account:
@@ -121,9 +119,6 @@ def update_accounts(account_id):
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
-
-# ... place you code here to DELETE an account ...
-
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
